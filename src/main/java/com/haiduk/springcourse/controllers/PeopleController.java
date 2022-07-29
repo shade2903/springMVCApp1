4a3d4322afecedfabad926aa807b001dae037d43
shade2903 <nighshade2903@gmail.com>
2022-07-29 15:18:12 +0300

    DAO+

--- src/main/java/com/haiduk/springcourse/controllers/PeopleController.java
+++ src/main/java/com/haiduk/springcourse/controllers/PeopleController.java
@@ -5,9 +5,10 @@ import com.haiduk.springcourse.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/people")
@@ -32,9 +33,37 @@ public class PeopleController {
     }
 
     @GetMapping("/new")
-    public String newPerson(Model model){
-        model.addAttribute("people",new Person());
+    public String newPerson(@ModelAttribute("person") Person person){
         return "people/new";
+    }
 
+    @PostMapping()
+    public String create(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "people/new";
+        }
+       personDAO.save(person);
+       return "redirect:/people";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id){
+        model.addAttribute("person",personDAO.show(id));
+        return "people/edit";
+    }
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult,
+                         @PathVariable("id") int id){
+        if(bindingResult.hasErrors()){
+            return "people/edit";
+        }
+        personDAO.update(id,person);
+        return "redirect:/people";
+    }
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id){
+        personDAO.delete(id);
+        return "redirect:/people";
     }
 }
